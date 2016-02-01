88d1120643d28e0a1f0297099ca49373503fdfab
jeffcampbell <jeffcampbell@meetup.com>
2016-01-31 19:43:41 -0500

    first commit

--- app.rb
+++ app.rb
@@ -18,9 +18,9 @@ post "/" do
   response = ""
 begin
   puts "[LOG] #{params}"
-  params[:text] = params[:text].sub(params[:trigger_word], "").strip.gsub(/:/, '')
+  params[:text] = params[:text].sub(params[:trigger_word], "")
   unless params[:token] != ENV["OUTGOING_WEBHOOK_TOKEN"]
-    response = { text: "Next Meetup:" }
+    response = { text: "From the collection:" }
     response[:attachments] = [ generate_attachment ]
     response[:username] = ENV["BOT_USERNAME"] unless ENV["BOT_USERNAME"].nil?
     response[:icon_emoji] = ENV["BOT_ICON"] unless ENV["BOT_ICON"].nil?
@@ -34,68 +34,31 @@ end
 def generate_attachment
   @user_query = params[:text]
 if @user_query.length == 0
-  uri = "https://api.meetup.com/2/events?group_id=#{ENV["MEETUP_GROUP_ID"]}&page=2&key=#{ENV["MEETUP_API_KEY"]}"
+  uri = "http://scrapi.org/random?fields=title,whoList/who/name,whenList/when/name,whatList/what/name,primaryImageUrl,url'"
 else
-  uri = "https://api.meetup.com/2/open_events?topic=#{@user_query}&zip=#{ENV["ZIP_CODE"]}&page=2&key=#{ENV["MEETUP_API_KEY"]}"
+  @user_query = @user_query.gsub(/ /, '%20'
+  uri = "http://scrapi.org/search/#{@user_query}?fields=title,whoList/who/name,whenList/when/name,whatList/what/name,primaryImageUrl,url'"
 end
   request = HTTParty.get(uri)
   puts "[LOG] #{request.body}"
 
   # Check for a nil response in the array
-  @firstresults = JSON.parse(request.body)["results"][0]
-  @secondresults = JSON.parse(request.body)["results"][1]
+  @scrapiresults = JSON.parse(request.body)[0]
 
-  # First Meetup
-  if @firstresults.nil?
-    response = { title: "No upcoming Meetups" }
+  # scrapi results
+  if @scrapiresults.nil?
+    response = { title: "No results" }
   else
-  # Check for venue information
-  if @firstresults["venue"]
-    @name = @firstresults["venue"]["name"]
-    @lat = @firstresults["venue"]["lat"]
-    @lon = @firstresults["venue"]["lon"]
-    firstlocation = "<http://www.google.com/maps/place/#{@lat},#{@lon}|#{@name}>"
-  else
-    firstlocation = "No location provided"
-  end
-  get_firstname = @firstresults["name"]
-  get_firsturl = @firstresults["event_url"]
-  get_firstrsvpcount = @firstresults["yes_rsvp_count"]
-  get_firstwaitlistcount = @firstresults["waitlist_count"]
-  raw_firsttime = @firstresults["time"].to_f / 1000
-  utc_firstoffset = @firstresults["utc_offset"].to_i / 1000
-  calc_firsttime = raw_firsttime + utc_firstoffset
-  final_firsttime = Time.at(calc_firsttime)
-  cldr_firsttime = final_firsttime.localize
-  get_firsttime = "#{cldr_firsttime.to_short_s} #{cldr_firsttime.to_date.to_full_s}"
+
+  get_title = @scrapiresults["title"]
+  get_who = @scrapiresults["whoList"]["who"]["name"]
+  get_when = @scrapiresults["whenList"]["when"]["name"]
+  get_what = @scrapiresults["whatList"]["what"][0]
+  get_imageurl = @scrapiresults["primaryImageUrl"]
+  get_url = @scrapiresults["url"]
   end
 
-  # Second Meetup
-  if @secondresults.nil?
-    get_secondname = ""
-    get_secondurl = ""
-    get_secondtime = ""
-    secondlocation = ""
-  else
-  # Check for venue information
-  if @secondresults["venue"]
-    @name = @secondresults["venue"]["name"]
-    @lat = @secondresults["venue"]["lat"]
-    @lon = @secondresults["venue"]["lon"]
-    secondlocation = "<http://www.google.com/maps/place/#{@lat},#{@lon}|#{@name}>"
-  else
-    secondlocation = "No location provided"
-  end
-  get_secondname = @secondresults["name"]
-  get_secondurl = @secondresults["event_url"]
-  raw_secondtime = @secondresults["time"].to_f / 1000
-  utc_secondoffset = @secondresults["utc_offset"].to_i / 1000
-  calc_secondtime = raw_secondtime + utc_secondoffset
-  final_secondtime = Time.at(calc_secondtime)
-  cldr_secondtime = final_secondtime.localize
-  get_secondtime = "#{cldr_secondtime.to_short_s} #{cldr_secondtime.to_date.to_full_s}"
-
-  response = { title: "#{get_firstname}", title_link: "#{get_firsturl}", text: "#{get_firsttime}\n#{firstlocation}", fields: [ { title: "RSVPs", value: "#{get_firstrsvpcount}", short: true }, { title: "Waitlist", value: "#{get_firstwaitlistcount}", short: true }, { title: "Following Meetup:", value: "<#{get_secondurl}|#{get_secondname}> - #{get_secondtime}", short: false } ] }
+  response = { title: "#{get_title}", title_link: "#{get_url}", image_url: "#{get_imageurl}", text: "Artist: #{get_who}", fields: [ { title: "Period", value: "#{get_when}", short: true }, { title: "Material", value: "#{get_what}", short: true } ] }
   end
 
 end
